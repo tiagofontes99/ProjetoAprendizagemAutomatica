2d834ad35857b03f758e0aaa9f1049452a574da7
tiagofontes99 <tiagofontes99@gmail.com>
2025-09-19 17:13:41 +0100

    funtions made

--- project/main.py
+++ project/main.py
@@ -2,11 +2,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 
 from project.models.trainModles import trainModels
-from project.mymodel import predict
-
+from project.mymodel import predict, bestmodel
 
 X_load = np.load('project/dataFiles/X_train.npy')
 y_load = np.load('project/dataFiles/Y_train.npy')
 X_train, X_test, y_train, y_test = train_test_split(X_load, y_load, test_size=0.2, random_state=42)
 trainModels(X_train, y_train)
-predict(X_test)
+#bestmodel(X_test, y_test)
+#print(predict(X_test))
--- project/mymodel.py
+++ project/mymodel.py
@@ -1,10 +1,29 @@
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+
 from project.models.Lasso import lassoPred
 from project.models.Polynomial import polyPred
 from project.models.Ridge import ridgePred
 
+def bestmodel(X_test, y_test):
+    gs_lasso = lassoPred(X_test)
+    gs_ridge = ridgePred(X_test)
+    gs_poly = polyPred(X_test)
+    print("\n[LASSO]")
+    print("MAE :", mean_absolute_error(X_test, y_test))
+    print("Mean Squared Error:", mean_squared_error(X_test, y_test))
+    print("R²  :", r2_score(X_test, y_test))
+
+    # Avaliaçao Ridge
+    print("\n[RIDGE]")
+    print("MAE :", mean_absolute_error(X_test, y_test))
+    print("Mean Squared Error:", mean_squared_error(X_test, y_test))
+    print("R²  :", r2_score(X_test, y_test))
+
+    # Avaliaçao Poly
+    print("\n[Poly]")
+    print("MAE :", mean_absolute_error(X_test, y_test))
+    print("Mean Squared Error:", mean_squared_error(X_test, y_test))
+    print("R²  :", r2_score(X_test, y_test))
 
 def predict(X_test):
-    lassopred = lassoPred(X_test)
-    ridgepred = ridgePred(X_test)
-    polypred = polyPred(X_test)
-    return
+    return lassoPred(X_test)
