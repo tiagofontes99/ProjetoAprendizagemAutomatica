4e6bce5ae0cc8472f2030eab5e608c2c8fd4dd5a
tiagofontes99 <tiagofontes99@gmail.com>
2025-09-21 14:57:43 +0100

    aplicação de um modelo polinomial

--- project/models/Polynomial.py
+++ project/models/Polynomial.py
@@ -1,31 +1,23 @@
 import numpy as np
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, LinearRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
 from project.pickle.pickleFun import loadFromPickle, saveToPickle
 
-
+poly= PolynomialFeatures(degree=4)
 def polyTrain(X_train, y_train):
-    # Polinomial regression
+    poly = PolynomialFeatures(degree=4)
+    polynomialx = poly.fit_transform(X_train)
 
-    pipe_poly = Pipeline([
-        ("poly", PolynomialFeatures(degree=1)),
-        ("scaler", StandardScaler()),
-        ("model", Ridge(random_state=42))
-    ])
-
-    param_poly = {
-        "model__alpha": np.logspace(-3, 3, 15)
-    }
-
-    gs_poly = GridSearchCV(pipe_poly, param_poly, scoring="neg_root_mean_squared_error", n_jobs=-1)
-    gs_poly.fit(X_train, y_train)
-    saveToPickle(gs_poly, "polyTrained")
+    poly.fit(polynomialx, y_train)
+    linearRegression = LinearRegression()
+    linearRegression.fit(polynomialx, y_train)
+    saveToPickle(linearRegression, "polyTrained")
 
 def polyPred(X_test):
     model = loadFromPickle("polyTrained")
-    y_pred = model.predict(X_test)
+    y_pred = model.predict(poly.fit_transform(X_test))
     return y_pred
 
--- project/mymodel.py
+++ project/mymodel.py
@@ -7,23 +7,23 @@ from project.models.Ridge import ridgePred
 def bestmodel(X_test, y_test):
     gs_lasso = lassoPred(X_test)
     gs_ridge = ridgePred(X_test)
-    gs_poly = polyPred(X_test)
+    poly = polyPred(X_test)
     print("\n[LASSO]")
-    print("MAE :", mean_absolute_error(X_test, y_test))
-    print("Mean Squared Error:", mean_squared_error(X_test, y_test))
-    print("R²  :", r2_score(X_test, y_test))
+    print("MAE :", mean_absolute_error(gs_lasso, y_test))
+    print("Mean Squared Error:", mean_squared_error(gs_lasso, y_test))
+    print("R²  :", r2_score(gs_lasso, y_test))
 
     # Avaliaçao Ridge
     print("\n[RIDGE]")
-    print("MAE :", mean_absolute_error(X_test, y_test))
-    print("Mean Squared Error:", mean_squared_error(X_test, y_test))
-    print("R²  :", r2_score(X_test, y_test))
+    print("MAE :", mean_absolute_error(gs_ridge, y_test))
+    print("Mean Squared Error:", mean_squared_error(gs_ridge, y_test))
+    print("R²  :", r2_score(gs_ridge, y_test))
 
     # Avaliaçao Poly
     print("\n[Poly]")
-    print("MAE :", mean_absolute_error(X_test, y_test))
-    print("Mean Squared Error:", mean_squared_error(X_test, y_test))
-    print("R²  :", r2_score(X_test, y_test))
+    print("MAE :", mean_absolute_error(poly, y_test))
+    print("Mean Squared Error:", mean_squared_error(poly, y_test))
+    print("R²  :", r2_score(poly, y_test))
 
 def predict(X_test):
     return lassoPred(X_test)
